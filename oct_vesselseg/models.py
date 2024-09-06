[FIX] Add CLI argument for random vessel ablation. Reposition epoch calculation to inside train_it()

oct_vesselseg/models.py
@@ -359,10 +359,16 @@ class UnetWrapper(nn.Module):
         utils.JsonTools(self.json_path).log(backbone_dict)
         self.trainee = self.load(backbone_dict, augmentation)
 
-    def train_it(self, synth_data_experiment_n, train_to_val: float = 0.8,
-                 batch_size: int = 1, epochs=1000,
+    def train_it(self,
+                 synth_data_experiment_n,
+                 synth_samples: int = -1,
+                 training_steps: int = int(1e5),
+                 train_to_val: float = 0.8,
+                 batch_size: int = 1,
                  check_val_every_n_epoch: int = 1,
-                 accumulate_gradient_n_batches: int = 1, num_workers: int = 1):
+                 accumulate_gradient_n_batches: int = 1,
+                 num_workers: int = 1
+                 ):
         """
         Train unet after defining or loading model.
 
@@ -370,35 +376,45 @@ class UnetWrapper(nn.Module):
         ----------
         synth_data_experiment_n : int
             Dataset that will be used for training model.
+        synth_samples : int
+            Number of samples in the combined training and testing set.
+            Default is -1 (all samples available).
+        training_steps : int
+            Number of training steps to take. Default is 100,000
         train_to_val : float
             Ratio of training data to validation data for training loop.
+            Default is 0.8
         batch_size : int
-            Number of volumes per batch.
-        epochs : int
-            Number of epochs in entire trainig loop.
+            Number of volumes per batch. Default is 1.
         check_val_every_n_epoch : int
             Number of times validation dice score is calculated (expressed in
-            epochs).
+            epochs). Default is 1.
         accumulate_gradient_n_batches : int
             Number of batches to compute before stepping optimizer.
-        i_max : float
-            Maximum intensity for synthesized volumes.
+        num_workers : int
+            Number of workers for torch dataloader. Default is 1.
         """
+        self.synth_samples = synth_samples
+        self.train_to_val = train_to_val
+        self.n_train = synth_samples * train_to_val
         self.batch_size = batch_size
-        self.epochs = epochs
         self.check_val_every_n_epoch = check_val_every_n_epoch
         self.accumulate_gradient_n_batches = accumulate_gradient_n_batches
         self.num_workers = num_workers
         self.exp_path = (f'{vesselseg_outdir}/synthetic_data'
                          f'/exp{synth_data_experiment_n:04d}')
+        self.epochs = int(
+            (training_steps * self.batch_size) // self.n_train)
+
+        print(f'Training for {self.epochs} epochs')
+
         # Init dataset
-        dataset = VesselLabelDataset(inputs=f'{self.exp_path}/*label*')
-        # Splitting up train and val sets
-        train_split = train_to_val
-        val_split = 1 - train_split
+        dataset = VesselLabelDataset(
+            inputs=f'{self.exp_path}/*label*', subset=self.synth_samples)
+        # Splitting up train and val sets with specified random seed
         seed = torch.Generator().manual_seed(42)
         self.train_set, self.val_set = random_split(
-            dataset, [train_split, val_split], seed)
+            dataset, [self.train_to_val, 1 - self.train_to_val], seed)
         # Logger and checkpoint stuff
         self.logger = TensorBoardLogger(
             self.output_path, self.model_dir, self.version_n)