260b883aa2b094252903d196567db6d669ec5218
EtienneChollet <etiennechollet2626@gmail.com>
2025-06-11 09:58:35 -0400

    [BUILD] Add general build version of cupy-cuda

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup, find_packages
 cuda_version = os.getenv('CUDA_VERSION', '11.6')
 
 # Strip the period from the version string (e.g., '11.6' -> '116')
-cuda_version = cuda_version.replace('.', '')
+cuda_major_version = cuda_version.split('.')[0]
 
 # Format version to get prebuilt wheel
-cupy_package = f'1{cuda_version}'
+cupy_package = f'cupy-cuda{cuda_major_version}x'
 
 setup(
     name='oct_vesselseg',
@@ -23,12 +23,14 @@ setup(
         "Paper Preprint": "https://arxiv.org/abs/2407.01419v1",
         "Source": "https://github.com/EtienneChollet/oct_vesselseg"
     },
+
     packages=find_packages(),
     entry_points={
             'console_scripts': [
                 'oct_vesselseg=oct_vesselseg:main.app'
             ]
         },
+
     install_requires=[
         cupy_package,
         'cppyy==2.3',
@@ -48,6 +50,7 @@ setup(
         'cyclopts',
         'cornucopia',
     ],
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
@@ -55,5 +58,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.9',
     ],
+
     python_requires='~=3.9',
 )
