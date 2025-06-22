a60aec10272d01424dbbf6044768f7243fff8e6b
EtienneChollet <etiennechollet2626@gmail.com>
2025-06-22 16:14:17 -0400

    [FEAT] Add more argument passthroughs

--- oct_vesselseg/client.py
+++ oct_vesselseg/client.py
@@ -5,7 +5,8 @@ Example
 -------
 python oct_vesselseg/client.py /autofs/cluster/octdata2/users/epc28/data/caroline_data/I46_Somatosensory_20um_averaging_new_mask_cleaned.nii
 """
-
+import pprint
+from typing import Literal
 import requests
 from cyclopts import App
 
@@ -16,6 +17,11 @@ app = App()
 def predict_volume(
     in_path: str,
     out_path: str | None = 'oct_vesselseg_output.nii.gz',
+    patch_size: int = 128, 
+    redundancy: int = 3,
+    pad_it: bool = True,
+    padding_method: Literal['reflect', 'replicate', 'constant'] = 'reflect',
+    normalize_patches: bool = True,
     api_url: str = 'http://127.0.0.1:8000/predict',
 ) -> None:
     """
@@ -28,6 +34,14 @@ def predict_volume(
     out_path : str | 'oct_vesselseg_output.nii.gz', optional
         Local path to save the returned bytes. If None, will print
         first 100 bytes to stdout.
+    patch_size : int
+        Size of Unet in each dimension.
+    redundancy: int
+        Redundancy factor for prediction overlap (default: 3).
+    padding_method : str
+        Method to pad the input tensor.
+    normalize_patches : bool
+        Optionally normalize each patch before prediction
     api_url : str, optional
         Full URL to the `/predict` endpoint, e.g.
         "http://127.0.0.1:8000/predict".
@@ -35,8 +49,14 @@ def predict_volume(
 
     data = {
         "in_path": in_path,
-        "out_path": out_path
+        "out_path": out_path,
+        "patch_size": patch_size,
+        "redundancy": redundancy,
+        "pad_it": str(pad_it).lower(),
+        "padding_method": padding_method,
+        "normalize_patches": str(normalize_patches).lower(),
     }
+    pprint.pprint(data)
 
     with requests.post(api_url, data=data, stream=True) as resp:
         resp.raise_for_status()
@@ -48,7 +68,9 @@ def predict_volume(
             except:
                 pass
 
-        #if out_path:
+        # print(resp.status_code)
+        # print(resp.text)
+        # if out_path:
         #    print(f"Saved prediction to {out_path!r}")
 
 
