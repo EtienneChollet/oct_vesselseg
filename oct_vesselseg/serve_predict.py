4128bef4265cdb4cc21ae999369e742d28c51f1e
EtienneChollet <etiennechollet2626@gmail.com>
2025-06-22 16:13:39 -0400

    [CHORE] Clean up serve_predict.py

--- oct_vesselseg/serve_predict.py
+++ oct_vesselseg/serve_predict.py
@@ -21,6 +21,7 @@ import shutil
 import threading
 from typing import Generator
 import asyncio
+from typing import Literal
 
 from enum import Enum
 from typing import Annotated
@@ -34,6 +35,7 @@ import torch
 from oct_vesselseg.server import run_job_in_thread, Queue, monitor_disconnect
 from oct_vesselseg.models import UnetWrapper
 from oct_vesselseg.data import RealOctPredict, RealOctConfig
+from oct_vesselseg.callbacks import CancelOnFlag
 
 
 # Initialize app
@@ -49,9 +51,18 @@ async def predict(
     request: Request,
     in_path: Annotated[str, Form()],
     out_path: Annotated[str, Form()],
+    patch_size: Annotated[int, Form()] = 128, 
+    redundancy: Annotated[int, Form()] = 3,
+    pad_it: Annotated[Literal['true', 'false'], Form()] = 'true',
+    padding_method: Annotated[str, Form()] = 'reflect',
+    normalize_patches: Annotated[Literal['true', 'false'], Form()] = 'true',
 ):
     queue = Queue()
     cancel_event = threading.Event()
+
+    # Convert string -> bool
+    pad_it = pad_it.lower() == "true"
+    normalize_patches = normalize_patches.lower() == "true"
 
     # Start the background job
     run_job_in_thread(
@@ -59,6 +70,12 @@ async def predict(
         queue=queue,
         cancel_event=cancel_event,
         in_path=in_path,
+        out_path=out_path,
+        patch_size=patch_size,
+        redundancy=redundancy,
+        pad_it=pad_it,
+        padding_method=padding_method,
+        normalize_patches=normalize_patches,
     )
 
     # Start a disconnect monitor in parallel
@@ -78,14 +95,24 @@ def prediction_job(
     cancel_event: threading.Event,
     in_path: str,
     out_path: str = None,
+    patch_size: int = 128, 
+    redundancy: int = 3,
+    pad_it: bool = True,
+    padding_method: str = 'reflect',
+    normalize_patches: bool = True,
+    #*args, **kwargs,
 ):
 
-    print("=== Job Started ===")
-    print(f"\nLoading volume: {in_path}\n")
+    print("=== Job Started ===\n")
+    print(f"Loading volume: {in_path}\n")
     print(f'Going to save to {out_path}\n')
 
     volume_tensor = nib.load(in_path).get_fdata()
     print(f"Shape: {volume_tensor.shape}\n")
+
+    callbacks = [
+        CancelOnFlag(cancel_event),
+    ]
 
     with torch.no_grad():
         unet = UnetWrapper(
@@ -99,84 +126,27 @@ def prediction_job(
         # Configuring prediction
         oct_config = RealOctConfig(
             input=in_path,
-            patch_size=128,
-            redundancy=3,
-            pad_it=True,
-            padding_method='reflect',
-            normalize=True,
+            patch_size=patch_size,
+            redundancy=redundancy,
+            pad_it=pad_it,
+            padding_method=padding_method,
+            normalize=normalize_patches,
         )
 
-        prediction = RealOctPredict(oct_config, trainee=unet.trainee)
+        prediction = RealOctPredict(
+            oct_config,
+            trainee=unet.trainee,
+            callbacks=callbacks,
+        )
+
         prediction.predict_on_all()
-
-    #for i in range(1000):
-    #    if cancel_event.is_set():
-    #        print(f"Cancelled at step {i}")
-    #        return
-    #    print(f"Working... step {i}")
-    #    time.sleep(0.5)
-    print("=== Job Completed ===")
-
-
-# @app.post("/predict")
-async def predict3333333(
-    in_path: Annotated[str, Form()],
-    out_path: Annotated[str, Form()],
-) -> StreamingResponse:
-    """
-    Segment a 3-D volume.
-
-    Parameters
-    ----------
-    in_path
-        NIfTI file (`.nii` / `.nii.gz`) containing volume of shape (D, H, W) or
-        (C, D, H, W).
-
-    Returns
-    -------
-    StreamingResponse
-        Binary payload with appropriate `Content-Type`.
-    """
-
-    async def streamer():
-
-        width = shutil.get_terminal_size(fallback=(80, 24)).columns
-        yield str("-" * width).encode()
-        yield f"\nLoading volume: {in_path}\n".encode()
-        print(f'Going to save to {out_path}')
-
-        volume_tensor = nib.load(in_path).get_fdata()
-        yield f"Shape: {volume_tensor.shape}\n".encode()
-
-        with torch.no_grad():
-            unet = UnetWrapper(
-                version_n=1,
-                model_dir='models',
-                device='cuda'
-            )
-
-            unet.load(type='best', mode='test')
-
-            # Configuring prediction
-            oct_config = RealOctConfig(
-                input=in_path,
-                patch_size=128,
-                redundancy=3,
-                pad_it=True,
-                padding_method='reflect',
-                normalize=True,
-            )
-
-            prediction = RealOctPredict(oct_config, trainee=unet.trainee)
-            # prediction.predict_on_all()
+        prediction.save_prediction(out_path)
 
         del unet, oct_config, volume_tensor, prediction
         torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
         gc.collect()
 
-        print('Finished the job!')
 
-    return StreamingResponse(
-        streamer(),
-        media_type="application/octet-stream"
-    )
+
+    print("=== Job Completed ===\n")
